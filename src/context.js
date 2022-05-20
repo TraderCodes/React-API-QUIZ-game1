ebae7263edd4a98d7f3732334dda00a09c3a3100
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-20 09:08:32 -0700

    🟢fetch api using axios

--- src/context.js
+++ src/context.js
@@ -15,6 +15,8 @@ const tempUrl =
 
 const AppContext = React.createContext();
 
+// =================================================================
+
 const AppProvider = ({ children }) => {
    // setup waiting before the question
    // Todo setup form to enter
@@ -22,10 +24,42 @@ const AppProvider = ({ children }) => {
    const [loading, setLoading] = useState(false);
    const [index, setIndex] = useState(0);
    const [question, setQuestion] = useState([]);
-  //  ! Show correct answer number
+   //  ! Show correct answer number
    const [correct, setCorrect] = useState(0);
-  // ! if failed to fetch display error
+   // ! if failed to fetch display error
    const [error, setError] = useState(false);
+   const [isModalOpen, setIsModalOpen] = useState(false);
+// todo FETCH 🟢
+   const fetchQuestion = async (url) => {
+      setLoading(true);
+      setWaiting(false);
+      const response = await axios(url).catch((error) => {
+         console.log(error);
+      });
+      if (response) {
+         const data = response.data.results;
+         // ! If data.length is bigger than 0 that means there is question to load
+         if (data.length > 0) {
+            setLoading(false);
+            setWaiting(false);
+            setQuestion(data); // pass the data into question
+            setError(false); //whipe error
+         } else {
+            //  ! If data.length is smaller than 0
+            setError(true);
+            setWaiting(true);
+         }
+      } else {
+         setWaiting(true);
+      }
+   };
+
+   //  start fetching with useeffcts
+   useEffect(() => {
+      fetchQuestion(tempUrl);
+   },[]);
+
+   //  =================================================================
    return (
       <AppContext.Provider
          value={{ waiting, loading, index, question, correct, error }}
@@ -34,6 +68,7 @@ const AppProvider = ({ children }) => {
       </AppContext.Provider>
    );
 };
+
 // make sure use
 export const useGlobalContext = () => {
    return useContext(AppContext);

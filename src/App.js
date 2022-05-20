51a1dea0fae9ca667365904135af94a0e9b2af6e
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-20 08:30:24 -0700

    ✔ setup useState and Loading + action on form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from 'react';
+import { useGlobalContext } from './context';
 
-import SetupForm from './SetupForm'
-import Loading from './Loading'
-import Modal from './Modal'
+import SetupForm from './SetupForm';
+import Loading from './Loading';
+import Modal from './Modal';
 function App() {
-  return <h2>quiz starter</h2>
+   const { waiting, loading, question, correct, index } = useGlobalContext();
+   // 👇when waiting return form for user to enter
+   if (waiting) {
+      return <SetupForm />;
+   }
+   if (loading) {
+      return <Loading />;
+   }
+   //  if is not loading or waiting show 🟢quiz app
+   return <main> quiz app</main>;
 }
 
-export default App
+export default App;
--- src/context.js
+++ src/context.js
@@ -1,24 +1,42 @@
-import axios from 'axios'
-import React, { useState, useContext, useEffect } from 'react'
+import axios from 'axios';
+import React, { useState, useContext, useEffect } from 'react';
 
 const table = {
-  sports: 21,
-  history: 23,
-  politics: 24,
-}
+   sports: 21,
+   history: 23,
+   politics: 24,
+};
 
-const API_ENDPOINT = 'https://opentdb.com/api.php?'
+const API_ENDPOINT = 'https://opentdb.com/api.php?';
 
-const url = ''
+const url = '';
+const tempUrl =
+   'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple';
 
-const AppContext = React.createContext()
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
+   // setup waiting before the question
+   // Todo setup form to enter
+   const [waiting, setWaiting] = useState(false);
+   const [loading, setLoading] = useState(false);
+   const [index, setIndex] = useState(0);
+   const [question, setQuestion] = useState([]);
+  //  ! Show correct answer number
+   const [correct, setCorrect] = useState(0);
+  // ! if failed to fetch display error
+   const [error, setError] = useState(false);
+   return (
+      <AppContext.Provider
+         value={{ waiting, loading, index, question, correct, error }}
+      >
+         {children}
+      </AppContext.Provider>
+   );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+   return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
